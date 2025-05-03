8c6413a84bf157cbadd153bc8c82345c1a0073f5
silvinor <366673+silvinor@users.noreply.github.com>
2025-05-03 21:49:55 +1000

    Update post_rules.mk

--- sr_version/post_rules.mk
+++ sr_version/post_rules.mk
@@ -25,8 +25,9 @@ QMK_VERSION_DATE_DAY := -1
 else
 QMK_VERSION_DATE := "$(QMK_GIT_DATE)"
 QMK_VERSION_DATE_YEAR := $(shell echo $(QMK_GIT_DATE) | cut -d- -f1)
-QMK_VERSION_DATE_MONTH := $(shell echo $(QMK_GIT_DATE) | cut -d- -f2)
-QMK_VERSION_DATE_DAY := $(shell echo $(QMK_GIT_DATE) | cut -d- -f3)
+QMK_VERSION_DATE_MONTH := $(shell expr $(shell echo $(QMK_GIT_DATE) | cut -d- -f2) + 0)
+QMK_VERSION_DATE_DAY := $(shell expr $(shell echo $(QMK_GIT_DATE) | cut -d- -f3) + 0)
+
 endif
 
 # Version
@@ -34,15 +35,26 @@ endif
 QMK_GIT_VERSION := $(shell git -C "$(TOP_DIR)" describe --tags 2>/dev/null)
 ifeq ($(QMK_GIT_VERSION),)
 # account for Vial missing git tag
-QMK_VERSION_MAJOR := $(shell echo $(QMK_VERSION_DATE_YEAR) | sed 's/.*\(..\)$$/\1/')
+QMK_VERSION_MAJOR := $(shell echo $(QMK_VERSION_DATE_YEAR) | cut -c3-4)
 QMK_VERSION_MINOR := $(QMK_VERSION_DATE_MONTH)
 QMK_VERSION_PATCH := $(QMK_VERSION_DATE_DAY)
+QMK_VERSION_HASH := $(shell git -C "$(TOP_DIR)" rev-parse --short=6 HEAD 2>/dev/null)
+ifeq ($(QMK_VERSION_HASH),)
+QMK_VERSION_HASH := "unknown"
+endif
+QMK_VERSION_DIRTY := $(shell git -C "$(TOP_DIR)" diff --quiet --ignore-submodules HEAD || echo "-dirty")
+QMK_VERSION_FIX := $(shell printf "%u.%u.%u-%s%s" \
+  $(QMK_VERSION_MAJOR) \
+  $(QMK_VERSION_MINOR) \
+  $(QMK_VERSION_PATCH) \
+  $(QMK_VERSION_HASH) \
+  $(QMK_VERSION_DIRTY))
 else
 QMK_VERSION_MAJOR := $(shell echo $(QMK_GIT_VERSION) | cut -d. -f1)
 QMK_VERSION_MINOR := $(shell echo $(QMK_GIT_VERSION) | cut -d. -f2)
 QMK_VERSION_PATCH := $(shell echo $(QMK_GIT_VERSION) | cut -d. -f3 | cut -d- -f1)
 endif
-QMK_VERSION_BCD := $(shell printf "0x%02X%02X%04X" \
+QMK_VERSION_BCD := $(shell printf "0x%02x%02x%04x" \
   $(QMK_VERSION_MAJOR) \
   $(QMK_VERSION_MINOR) \
   $(QMK_VERSION_PATCH))
@@ -52,6 +64,13 @@ QMK_VERSION_BCD := $(shell printf "0x%02X%02X%04X" \
 $(shell echo "" >> $(GENERATED_VERSION_H_FILE))
 $(shell echo "/* --- Liberally meddled with by the SilvinoR/SR_Version Community Module --- */" >> $(GENERATED_VERSION_H_FILE))
 $(shell echo "" >> $(GENERATED_VERSION_H_FILE))
+ifneq ($(QMK_VERSION_FIX),)
+$(shell echo "#ifdef QMK_VERSION" >> $(GENERATED_VERSION_H_FILE))
+$(shell echo "    // Guessing you're in Vial" >> $(GENERATED_VERSION_H_FILE))
+$(shell echo "#    undef QMK_VERSION" >> $(GENERATED_VERSION_H_FILE))
+$(shell echo "#    define QMK_VERSION \"$(QMK_VERSION_FIX)\"" >> $(GENERATED_VERSION_H_FILE))
+$(shell echo "#endif" >> $(GENERATED_VERSION_H_FILE))
+endif
 $(shell echo "#define QMK_VERSION_BCD $(QMK_VERSION_BCD)" >> $(GENERATED_VERSION_H_FILE))
 $(shell echo "#define QMK_VERSION_MAJOR $(QMK_VERSION_MAJOR)" >> $(GENERATED_VERSION_H_FILE))
 $(shell echo "#define QMK_VERSION_MINOR $(QMK_VERSION_MINOR)" >> $(GENERATED_VERSION_H_FILE))
