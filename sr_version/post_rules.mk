c1c9f4758f451412dd3558f54bc946b74d6d783e
silvinor <366673+silvinor@users.noreply.github.com>
2025-05-03 20:44:32 +1000

    Update post_rules.mk

--- sr_version/post_rules.mk
+++ sr_version/post_rules.mk
@@ -10,12 +10,14 @@ $(GENERATED_VERSION_H_FILE):
 
 # Info
 
-$(info Inappropriately touching the $(GENERATED_VERSION_H_FILE) file.)
+DUMMY := $(shell printf "\033[1;34mGently poking at the $(GENERATED_VERSION_H_FILE) file...\033[0m\n")
+$(info $(DUMMY))
 
 # Date
 
 QMK_GIT_DATE := $(shell git -C "$(TOP_DIR)" log -1 --date=format:'%Y-%m-%d' --format=%cd 2>/dev/null)
 ifeq ($(QMK_GIT_DATE),)
+# not on git, or git tag broken
 QMK_VERSION_DATE := "unknown"
 QMK_VERSION_DATE_YEAR := -1
 QMK_VERSION_DATE_MONTH := -1
@@ -31,7 +33,8 @@ endif
 
 QMK_GIT_VERSION := $(shell git -C "$(TOP_DIR)" describe --tags 2>/dev/null)
 ifeq ($(QMK_GIT_VERSION),)
-QMK_VERSION_MAJOR := $(QMK_VERSION_DATE_YEAR)
+# account for Vial missing git tag
+QMK_VERSION_MAJOR := $(shell echo $(QMK_VERSION_DATE_YEAR) | sed 's/.*\(..\)$$/\1/')
 QMK_VERSION_MINOR := $(QMK_VERSION_DATE_MONTH)
 QMK_VERSION_PATCH := $(QMK_VERSION_DATE_DAY)
 else
@@ -39,12 +42,17 @@ QMK_VERSION_MAJOR := $(shell echo $(QMK_GIT_VERSION) | cut -d. -f1)
 QMK_VERSION_MINOR := $(shell echo $(QMK_GIT_VERSION) | cut -d. -f2)
 QMK_VERSION_PATCH := $(shell echo $(QMK_GIT_VERSION) | cut -d. -f3 | cut -d- -f1)
 endif
+QMK_VERSION_BCD := $(shell printf "0x%02X%02X%04X" \
+  $(QMK_VERSION_MAJOR) \
+  $(QMK_VERSION_MINOR) \
+  $(QMK_VERSION_PATCH))
 
 # Output
 
 $(shell echo "" >> $(GENERATED_VERSION_H_FILE))
-$(shell echo "/* --- Added by the SilvinoR/SR_Version Community Module --- */" >> $(GENERATED_VERSION_H_FILE))
+$(shell echo "/* --- Liberally meddled with by the SilvinoR/SR_Version Community Module --- */" >> $(GENERATED_VERSION_H_FILE))
 $(shell echo "" >> $(GENERATED_VERSION_H_FILE))
+$(shell echo "#define QMK_VERSION_BCD $(QMK_VERSION_BCD)" >> $(GENERATED_VERSION_H_FILE))
 $(shell echo "#define QMK_VERSION_MAJOR $(QMK_VERSION_MAJOR)" >> $(GENERATED_VERSION_H_FILE))
 $(shell echo "#define QMK_VERSION_MINOR $(QMK_VERSION_MINOR)" >> $(GENERATED_VERSION_H_FILE))
 $(shell echo "#define QMK_VERSION_PATCH $(QMK_VERSION_PATCH)" >> $(GENERATED_VERSION_H_FILE))
